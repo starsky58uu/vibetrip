783258e7bcce09c971fc326331d69373cee6a9a5
Ultra Sheep <sheepdesnay@gmail.com>
2026-04-13 00:00:41 +0800

    docs: clean up docs

--- App.jsx
+++ App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, StyleSheet, StatusBar, ActivityIndicator } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-// 引入我們剛剛拆分的模組
 import { themeColors } from './src/constants/theme';
 import { useFontsLoaded } from './src/hooks/useFontsLoaded';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -22,7 +21,6 @@ export default function App() {
     <SafeAreaProvider>
       <StatusBar barStyle="light-content" backgroundColor={themeColors.background} />
       <SafeAreaView style={styles.appView} edges={['top', 'left', 'right']}>
-        {/* 所有的路由和頁面邏輯都交給 AppNavigator 處理 */}
         <AppNavigator />
       </SafeAreaView>
     </SafeAreaProvider>
--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -11,9 +11,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* 將 TabNavigator 作為主要入口 */}
         <Stack.Screen name="MainTabs" component={TabNavigator} />
-        {/* 其他需要被堆疊的獨立頁面放這裡 */}
         <Stack.Screen name="WeatherDetail" component={WeatherDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
